645e3b29dfb648a25876e47115fc19b1c6cc9154
Jose Fabio <fabio.7.45@hotmail.com>
2025-11-05 23:35:07 -0300

    interface e adiciao de veiculos

--- obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372dc9b1ccffe6f18e8ecd5c9f1b2aefa8d8e2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
